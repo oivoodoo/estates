930738cac3ac88ef79cb172f099cc7999e784f11
Zakhar Slabodnik <zakhar.slabodnik@gmail.com>
2013-05-16 17:40:36 +0300

    Use pg

--- Gemfile
+++ Gemfile
@@ -3,9 +3,6 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0.rc1'
 
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
-
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0.rc1'
 
@@ -30,6 +27,7 @@ gem 'jbuilder', '~> 1.0.1'
 gem 'haml'
 gem 'devise'
 gem 'protected_attributes'
+gem 'pg'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.new(:email => "admin@estates.com",
+  :password => "12345678",
+  :password_confirmation => "12345678")
+  admin.role = 'admin'
+  admin.save!
