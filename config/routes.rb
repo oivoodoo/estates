91b2bc9af5481ae5170b3e6fb7e2f317064f6879
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-05-16 16:09:23 +0300

    create admin page, add admin route for the app

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Estates::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  resources :admin
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
