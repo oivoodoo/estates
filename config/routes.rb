0e574573ae0afd4d01327a15d5ffb167484fddca
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-05-20 12:32:55 +0300

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Estates::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
   resources :admin, only: :index
-  resources :projects
+  resources :projects, only: :index
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
