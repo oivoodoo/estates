4611872ea94ae8492b3c6f6dcf4fb0ec9c6c9822
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-05-20 14:56:14 +0300

    test success

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 describe ProjectsController do
-	context "on show projects page" do
-		context "with existing project" do
+	describe "get :show" do
+		context "with project" do
+			let!(:projects) { 2.times.map { create(:project) } }
+
 			describe '#index' do
 		    before { get :index }
 		    
 		    it { should respond_with(:success) }
-		    it { assigns(:project).should == @project }
-		  end
+		  
+			  it 'should pick up items' do
+			    collection = assigns(:projects) 
+				  expect(collection).to have(2).items
+				  expect(collection).to include(projects[0])
+				  expect(collection).to include(projects[1])
+				end
+			end
 		end
 	end
 end
