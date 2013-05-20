1d7ddc5358f5b7fe8eb9ee970de4cd02ad802c03
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-05-20 12:06:11 +0300

    corrected controller file, add test o projects controller

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe ProjectsController do
-
+	context "on show projects page" do
+		context "with existing post" do
+			describe '#index' do
+		    before { get :index }
+		    
+		    it { should respond_with(:success) }
+		  end
+		end
+	end
 end
