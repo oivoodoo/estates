9a6e368f7383a6792f02f6178b2509dab8a0042e
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-05-20 15:00:30 +0300

    remove action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,8 +2,4 @@ class ProjectsController < ApplicationController
 	def index
 		@projects = Project.paginate(:page => params[:page])
 	end
-
-	def project_params
-		params.require(:project).permit(:name, :price, :owner, :description)
-	end
 end
