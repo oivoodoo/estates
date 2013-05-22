f866a66656e7af5c599389298886df650b280113
Alexandr Korsak <alex.korsak@gmail.com>
2013-05-22 22:43:56 +0300

    Add few specs for the facebook provider

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-	def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+  def facebook
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
@@ -12,3 +11,4 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,31 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+    :recoverable, :rememberable, :trackable, :validatable,
+    :omniauthable, :omniauth_providers => [:facebook]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :provider, :uid
+
+  validates :name, presence: true
 
   def role?(r)
     self.role == r
   end
 
-  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-	  user = User.where(:provider => auth.provider, :uid => auth.uid).first
-	  unless user
-	    user = User.create(name:auth.extra.raw_info.name, # TODO: add column name
-	                         provider:auth.provider,
-	                         uid:auth.uid,
-	                         email:auth.info.email,
-	                         password:Devise.friendly_token[0,20]
-	                         )
-	  end
-	  user
-	end
+  def self.find_for_facebook_oauth(auth)
+    user = User.find_by(provider: auth.provider, uid: auth.uid)
 
-	def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
+    unless user
+      user = User.create(
+        name:     auth.extra.raw_info.name,
+        provider: auth.provider,
+        uid:      auth.uid,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0,20]
+      )
     end
+
+    user
   end
 end
 
