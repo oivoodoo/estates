acd11e555c920501378499383bc06a8476cc25d0
Alexandr Korsak <alex.korsak@gmail.com>
2013-05-22 10:57:08 +0300

    Hide admin link to normal users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
   describe '#role?' do
-    context 'admin user' do
+    describe '#admin' do
       let(:admin) { create(:admin) }
 
       it 'should be admin' do
@@ -10,13 +10,23 @@ describe User do
       end
     end
 
-    context 'normal user' do
+    context '#user' do
       let(:user) { create(:user) }
 
       it 'should be normal user' do
         expect(user.role?(:admin)).to be_false
       end
     end
+  end
+end
+
+describe User do
+  context '#admin' do
+    let(:admin) { create(:admin) }
+
+    subject { Ability.new(admin) }
+
+    it { should be_able_to(:manage, :all) }
   end
 end
 
