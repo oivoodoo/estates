7bf8b33c7856633f4d29c7b59464ca858f11666d
Alexandr Korsak <alex.korsak@gmail.com>
2013-05-23 10:42:59 +0300

    Render list of the real estates on the home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ end
     p.owner       = "Owner #{n}"
     p.price       = "12345678"
     p.description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+    p.image       = File.open(Rails.root.join("public/images/house.jpg"))
   end
 end
 
