c5f30083ff12c9f3e85ba23ce82f6cffb70df189
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-06-10 11:54:19 +0300

    print users list on the home page

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,8 +2,20 @@ require 'spec_helper'
 
 describe HomeController do
   describe '#index' do
-    before { get :index }
+    context 'with users' do
+      before { logged_in(:user) }
+      let!(:users) { create_list(:user, 2) }
 
-    it { should respond_with(:success) }
+      before { get :index }
+
+      it { should respond_with(:success) }
+
+      it 'should pick up items' do
+        collection = assigns(:users)
+        expect(collection).to have(2).items
+        expect(collection).to include(users[0])
+        expect(collection).to include(users[1])
+      end
+    end
   end
 end
