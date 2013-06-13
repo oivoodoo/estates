c09b80d42401f34190b35567ea4501eb15696fdb
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-06-13 12:00:51 +0300

    now user can send messages to other user

---
+++ app/controllers/users/messages_controller.rb
@@ -0,0 +1,8 @@
+class Users::MessagesController < UsersController
+  def create
+    @user = User.find(params[:user_id])
+    current_user.send_message(@user, params[:message])
+
+    redirect_to @user, notice: "Message was sent"
+  end
+end
