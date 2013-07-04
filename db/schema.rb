5bf1be1bbebbcf65a05be03f10379a953b192101
Alexandr Korsak <alex.korsak@gmail.com>
2013-07-04 19:00:32 +0300

    Add location settings to the form page

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130704152050) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "authentications", force: true do |t|
     t.string   "provider",   null: false
     t.string   "uid",        null: false
