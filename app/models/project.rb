265bbe3da43ee0a2f3defcd590973353dce27926
Alexandr Korsak <alex.korsak@gmail.com>
2013-07-04 22:51:54 +0300

    Format project code

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,30 +1,31 @@
 class Project < ActiveRecord::Base
-	acts_as_commentable
-	acts_as_taggable
+  acts_as_commentable
+  acts_as_taggable
 
   geocoded_by :address
   after_validation :geocode
 
-	has_many :followers
-	has_many :users, through: :followers
-	
+  has_many :followers
+  has_many :users, through: :followers
+
   mount_uploader :image, ImageUploader
 
-	validates :name, :price, :owner, presence: true
+  validates :name, :price, :owner, presence: true
 
-	self.per_page = 8
+  self.per_page = 8
 
-	def followed_by!(user)
-		unless users.exists?(user.id)
-			users << user
-		end
-	end
+  def followed_by!(user)
+    unless users.exists?(user.id)
+      users << user
+    end
+  end
 
-	def self.search(query)
-		if query.present?
-    	where('name @@ :q or description @@ :q', q: query)
+  def self.search(query)
+    if query.present?
+      where('name @@ :q or description @@ :q', q: query)
     else
-    	scoped
+      scoped
     end
   end
 end
+
