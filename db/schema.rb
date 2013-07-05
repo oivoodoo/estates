d4a33f5285d089ab2cd1847cb0ee41a7a5066b8b
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-07-05 17:42:01 +0300

    merged

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20130705085442) do
-=======
 ActiveRecord::Schema.define(version: 20130705132308) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
->>>>>>> master
 
   create_table "authentications", force: true do |t|
     t.string   "provider",   null: false
@@ -135,12 +128,13 @@ ActiveRecord::Schema.define(version: 20130705132308) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "",        null: false
-    t.string   "encrypted_password",     limit: 128, default: "",        null: false
+    t.string   "email",                  default: "",        null: false
+    t.string   "name",                   default: "",        null: false
+    t.string   "encrypted_password",     default: "",        null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -148,8 +142,7 @@ ActiveRecord::Schema.define(version: 20130705132308) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
-    t.string   "name",                                                   null: false
-    t.string   "status",                             default: "pending"
+    t.string   "status",                 default: "pending"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
