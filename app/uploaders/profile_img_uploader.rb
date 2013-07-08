bac20337a685805d72a60af3611e3fd0d3652203
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-07-08 11:12:06 +0300

    change storage to active_record

--- app/uploaders/profile_img_uploader.rb
+++ app/uploaders/profile_img_uploader.rb
@@ -11,7 +11,7 @@ class ProfileImgUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :active_record
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
