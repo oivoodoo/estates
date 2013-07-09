3c1246d3a238a5a9d4be15cf5c6c2cb62bac0704
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-07-09 17:45:47 +0300

    removed file

--- db/migrate/20130709130916_change_column_street_city_country_for_projects.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnStreetCityCountryForProjects < ActiveRecord::Migration
-  def change
-    change_column :projects, :street, :string, default: "Slobodskaya 91"
-    change_column :projects, :city, :string, default: "Minsk"
-    change_column :projects, :country, :string, default: "Belarus"
-  end
-end
