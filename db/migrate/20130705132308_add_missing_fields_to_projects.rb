076a388dea70d0432b6deeefda149b215032bc78
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-07-09 15:57:25 +0300

    default fields for visual preview page

--- db/migrate/20130705132308_add_missing_fields_to_projects.rb
+++ db/migrate/20130705132308_add_missing_fields_to_projects.rb
@@ -2,7 +2,7 @@ class AddMissingFieldsToProjects < ActiveRecord::Migration
   def change
     add_column :projects, :property, :text
     add_column :projects, :strength, :text
-    add_column :projects, :investment_type, :text
+    add_column :projects, :investment_type, :text, :default => "loan"
     add_column :projects, :investment_term, :text
   end
 end
--- db/migrate/20130705144709_add_address_fields_to_projects.rb
+++ db/migrate/20130705144709_add_address_fields_to_projects.rb
@@ -1,7 +1,7 @@
 class AddAddressFieldsToProjects < ActiveRecord::Migration
   def change
-    add_column :projects, :street, :string
-    add_column :projects, :city, :string
-    add_column :projects, :country, :string
+    add_column :projects, :street, :string, default: "Slobodskaya 91" 
+    add_column :projects, :city, :string, default: "Minsk" 
+    add_column :projects, :country, :string, default: "Belarus" 
   end
 end
