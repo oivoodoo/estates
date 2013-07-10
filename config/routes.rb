95e23e41eeb30e509f8d14edcbe6445767df3c2b
Alexandr Korsak <alex.korsak@gmail.com>
2013-07-10 14:43:49 +0300

    Add basic blog

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Estates::Application.routes.draw do
   end
 
   get 'dashboard' => 'dashboard#index'
+  get 'blog' => 'blog#index', as: :blog
 
   resources :project_tags, only: :index
   get 'tags/:tag', to: 'project_tags#index', as: :tag
@@ -40,53 +41,14 @@ Estates::Application.routes.draw do
 
   namespace :admin do
     resources :projects
+    resources :posts
 
     resources :users do
       resources :messages, only: :create
     end
   end
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
   mount Ckeditor::Engine => '/ckeditor'
   mount GetVersion::Web => '/'
 end
+
