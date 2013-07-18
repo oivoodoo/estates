61933f43e68be5983b9107be9186d8af4f8000d3
Alexandr Korsak <alex.korsak@gmail.com>
2013-07-18 23:24:39 +0300

    enable debug assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,6 @@ Estates::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.debug = true
 end
