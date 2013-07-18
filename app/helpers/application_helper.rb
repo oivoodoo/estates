dc917111618fa744842a5aba706dbccbdedf95dd
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-07-18 19:11:22 +0300

    added elseif for helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,11 @@ module ApplicationHelper
   def layout_class
     if action_name == 'show'
       "project"
-    else
+    elsif
       controller_name == 'project_tags'
       "projects"
+    else
+      controller_name
     end
   end
 end
