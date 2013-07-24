64f2e186444570e5b31137eb3505d88a69fe2c6a
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-07-24 10:40:36 +0300

    pulled value from per_share

--- app/assets/javascripts/calculateTotalPurchase.js
+++ app/assets/javascripts/calculateTotalPurchase.js
@@ -1,7 +1,7 @@
 $(function() {
    $('#quantity').focus().keyup(function() {
       var quantity = $('#quantity').val();
-      var per_share = $('#per_share').data();
+      var per_share = $('#per_share').data('value');
       var result = (quantity * per_share);
 
       $("#result").html(result);
