cc451fd5eb7dd8dae40a75d9ecc98d49bee73a86
Alexandr Korsak <alex.korsak@gmail.com>
2013-08-07 18:44:40 +0300

    Add invests to the admin pages

---
+++ app/admin/invest.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Invest do
+  index do
+    column :project
+    column :created_at
+    default_actions
+  end
+
+  filter :project
+  filter :created_at
+
+  controller do
+    def permitted_params
+      params.permit!
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130807141200) do
+ActiveRecord::Schema.define(version: 20130807153517) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -171,6 +174,7 @@ ActiveRecord::Schema.define(version: 20130807141200) do
     t.integer  "shares",              default: 1,          null: false
     t.string   "risks",               default: ""
     t.text     "manager"
+    t.text     "short_description"
   end
 
   create_table "sessions", force: true do |t|
