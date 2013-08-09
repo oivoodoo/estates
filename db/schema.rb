dd4d881183a467e91bcac0ffc43e93f374d4a4ac
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-08-09 10:26:13 +0300

    corrected the deficiencies, added default image

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130808165535) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
     t.text     "body"
@@ -174,8 +171,8 @@ ActiveRecord::Schema.define(version: 20130808165535) do
     t.integer  "shares",              default: 1,          null: false
     t.string   "risks",               default: ""
     t.text     "manager"
-    t.text     "short_description"
     t.float    "raised"
+    t.text     "short_description"
   end
 
   create_table "sessions", force: true do |t|
