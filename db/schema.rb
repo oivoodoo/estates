c1d6d7092bb022b7487de55cfa48a4776f7c659c
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-08-16 12:07:52 +0300

    corrected invests css investment page

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130815190821) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
     t.text     "body"
@@ -266,7 +263,6 @@ ActiveRecord::Schema.define(version: 20130815190821) do
     t.string   "google_plus_link"
     t.string   "linkedin_link"
     t.string   "social_avatar_url"
-    t.string   "uid"
     t.float    "latitude"
     t.float    "longitude"
   end
