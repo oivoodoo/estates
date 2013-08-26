8a8839dea67d85ad13fd52196eaf7c9f870173f3
Alexandr Korsak <alex.korsak@gmail.com>
2013-08-26 12:06:29 +0300

    Change names in the admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
-  content :title => proc{ "Welcome to Estates Dashboard." } do
+  content :title => proc{ "Welcome to Flockcapital Dashboard." } do
     # Here is an example of a simple dashboard with columns and panels.
     #
     columns do
