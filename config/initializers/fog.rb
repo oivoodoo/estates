158d87b1f01b34e0d4b90ebbd6b0692dacd8386f
Alexandr Korsak <alex.korsak@gmail.com>
2013-08-26 11:56:04 +0300

    Bucket based on the env

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,11 +1,11 @@
-if Rails.env.production?
+if Rails.env.production? || Rails.env.staging?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['s3_key'],
       :aws_secret_access_key  => ENV['s3_access'],
     }
-    config.fog_directory  = 'estates_production'
+    config.fog_directory  = "estates_#{Rails.env}"
     config.fog_public     = false
   end
 end
