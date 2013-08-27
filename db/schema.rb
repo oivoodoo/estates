8cda9ca2a918e7ee7e1dfa517f00b822ed6e558d
Alexandr Korsak <alex.korsak@gmail.com>
2013-08-27 14:53:35 +0300

    Add indexes for the activities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130826122142) do
+ActiveRecord::Schema.define(version: 20130827115003) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -40,6 +43,9 @@ ActiveRecord::Schema.define(version: 20130826122142) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
 
   create_table "admin_users", force: true do |t|
     t.string   "email",                  default: "", null: false
