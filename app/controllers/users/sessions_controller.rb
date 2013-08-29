7b9ffb7d54cc1714f1a9885cd2b1934deecaf9db
Artem Slabodnik <artem.slabodnik@gmail.com>
2013-08-29 13:41:41 +0300

    now users will redirected to new user sessions path when run admin path

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,15 +1,33 @@
 class Users::SessionsController < Devise::SessionsController
-  layout false
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
 
+    if request.xhr?
+      # render devise sessions new form instead of new.js.erb of user users/sessions
+      render :new_form, :layout => false
+    else
+      render :html_form
+    end
+  end
   def create
     self.resource = warden.authenticate(auth_options)
     if self.resource.present?
       sign_in(resource_name, resource)
       flash.now[:success] = I18n.t('devise.sessions.signed_in')
+
+      unless request.xhr?
+        redirect_to root_path
+      end
     else
       flash.now[:errors] = I18n.t('devise.failure.invalid')
-      render :new
+
+      if request.xhr?
+        # render users/sessions/new.js.erb with errors
+        render :new, :layout => false
+      else
+        render :html_form
+      end
     end
   end
 end
-
