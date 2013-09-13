c1a30da8627085ae955ce45936d7c25de6f11246
Alexandr Korsak <alex.korsak@gmail.com>
2013-09-13 12:33:23 +0300

    Remove white list for the avatars of the social networks

--- app/uploaders/google_plus_avatar_uploader.rb
+++ app/uploaders/google_plus_avatar_uploader.rb
@@ -35,11 +35,5 @@ class GooglePlusAvatarUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [179,179]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
 end
 
